1979d48aa5cdd560f74bf0c780119a3ca55b0f20
kajamalan15 <it22113672@my.sliit.lk>
2025-05-29 12:57:05 +0530

    Fixed requirements.txt path for Streamlit Cloud

--- setup.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p ~/.streamlit
-echo "[general]
-email = \"your-email@example.com\"
-" > ~/.streamlit/credentials.toml
-sudo apt-get update
-sudo apt-get install -y libfreetype6-dev libpng-dev
